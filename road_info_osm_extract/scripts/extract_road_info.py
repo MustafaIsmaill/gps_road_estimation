c82778283be62f6e18c4e83564420489b065ee7f
MustafaIsmail <mustafa.el-sayed@hotmail.com>
2018-11-14 21:19:17 +0100

    added comments

--- road_info_osm_extract/scripts/extract_road_info.py
+++ road_info_osm_extract/scripts/extract_road_info.py
@@ -10,12 +10,16 @@ if __name__ == '__main__':
 
 	try:
 
+		#package inputs
+		#edit these 3 variables to match your criterea
 		place_name = 'leganes tecnologico (fase 1) ,leganes, madrid,spain'
 		node_name = 'road_info_extracter'
 		publish_rate = 0.1 # 0.1Hz
 
+		#create a map object
 		map = map_extract(node_name, place_name, publish_rate)
 
+		#publish road information
 		map._publish_road_info()
 
 	except rospy.ROSInterruptException:
--- road_info_osm_extract/scripts/map_extract.py
+++ road_info_osm_extract/scripts/map_extract.py
@@ -42,72 +42,87 @@ class map_extract:
 		    print("list of roads published")
 		    self._publish_rate.sleep()
 
+	#returns the x-coordinate of the start node
 	def _get_start_x(self, edge_id):
 
 		start_node_id = self._edges[:].u[edge_id]
 		return self._nodes[:].x[start_node_id]
 
+	#returns the y-coordinate of the start node
 	def _get_start_y(self, edge_id):
 
 		start_node_id = self._edges[:].u[edge_id]
 		return self._nodes[:].y[start_node_id]
 
+	#returns the x-coordinate of the end node
 	def _get_end_x(self, edge_id):
 
 		start_node_id = self._edges[:].v[edge_id]
 		return self._nodes[:].x[start_node_id]
 
+	#returns the y-coordinate of the end node
 	def _get_end_y(self, edge_id):
 
 		start_node_id = self._edges[:].v[edge_id]
 		return self._nodes[:].y[start_node_id]
 
+	#returns the latitude of the start node
 	def _get_start_lat(self, edge_id):
 
 		start_node_id = self._edges[:].u[edge_id]
 		return self._nodes[:].lat[start_node_id]
 
+	#returns the longitude of the start node
 	def _get_start_lon(self, edge_id):
 
 		start_node_id = self._edges[:].u[edge_id]
 		return self._nodes[:].lon[start_node_id]
 
+	#returns the latitude of the end node
 	def _get_end_lat(self, edge_id):
 
 		start_node_id = self._edges[:].v[edge_id]
 		return self._nodes[:].lat[start_node_id]
 
+	#returns the longitude of the end node
 	def _get_end_lon(self, edge_id):
 
 		start_node_id = self._edges[:].v[edge_id]
 		return self._nodes[:].lon[start_node_id]
 
+	#returns the length of the edge or road
 	def _get_edge_length(self, edge_id):
 
 		return self._edges[:].length[edge_id]
 
+	#returns a boolean indicating whether a road is a one way or not
 	def _get_edge_direction(self, edge_id):
 
 		direction = self._edges[:].oneway[edge_id]
 		direction = float(direction)
 		return direction
 
+	#parses the edges and nodes into readable lists
 	def _parse_road_info(self):
 
+		#loop through all roads or "edges"
 		for i in range(0,len(self._edges)):
 			
+			#create a msg of the type "road"
 			edge = road()
 
+			#fill the road info into the "road" message
 			edge.info.insert(0, i)	#road ID
-			edge.info.insert(1, self._get_start_x(i))
-			edge.info.insert(2, self._get_start_y(i))
-			edge.info.insert(3, self._get_start_lat(i))
-			edge.info.insert(4, self._get_start_lon(i))
-			edge.info.insert(5, self._get_end_x(i))
-			edge.info.insert(6, self._get_end_y(i))
-			edge.info.insert(7, self._get_end_lat(i))
-			edge.info.insert(8, self._get_end_lon(i))
-			edge.info.insert(9, self._get_edge_length(i))
-			edge.info.insert(10, self._get_edge_direction(i))
+			edge.info.insert(1, self._get_start_x(i))	#x-coordinate of start node
+			edge.info.insert(2, self._get_start_y(i))	#y-coordinate of start node
+			edge.info.insert(3, self._get_start_lat(i))	#latitude of start node
+			edge.info.insert(4, self._get_start_lon(i))	#longitude of start node
+			edge.info.insert(5, self._get_end_x(i))		#x-coordinate of end node
+			edge.info.insert(6, self._get_end_y(i))		#y-coordinate of end node
+			edge.info.insert(7, self._get_end_lat(i))	#latitude of end node
+			edge.info.insert(8, self._get_end_lon(i))	#longitude of end node
+			edge.info.insert(9, self._get_edge_length(i))	#length of road in meters
+			edge.info.insert(10, self._get_edge_direction(i))	#one way road or not
 
+			#add this road to the list of roads
 			self._list_of_roads.roads.insert(i, edge)
